cfa894edc5124053051656cea52630501cc59199
Sulto4 <meleru.radu4@yahoo.com>
2025-12-13 01:11:50 +0200

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
         'revista-gold': '#C6A667',
       },
       fontFamily: {
-        'serif': ['Playfair Display', 'serif'],
+        'serif': ['Lora', 'serif'],
         'sans': ['Inter', 'sans-serif'],
       },
       fontSize: {
